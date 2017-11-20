9e8d7ed45e859016bbd8a3cfc2af83d41c9629e7
dwasyluk <FUnstuff1>
2017-11-20 00:41:54 -0500

    revert tests

--- dist/swagger-spec.js
+++ dist/swagger-spec.js
@@ -3,7 +3,7 @@ var swaggerSpec =
   "swagger" : "2.0",
   "info" : {
     "version" : "1.1.2",
-    "title" : "Syscoin API V"
+    "title" : "Syscoin API"
   },
   "host" : "localhost:8001",
   "tags" : [ {
